36b4a7a5b9736e9e58c01983cf8eb35c5a3f77df
John Barbieri <joao.barbieri.ctr@brightside.com>
2025-06-29 01:23:19 -0300

    feat: implement search functionality in RecipesController and update index view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,13 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.order(created_at: :desc).page(params[:page]).per(20)
+    @recipes = if params[:query]
+                 Recipe
+                   .where(
+                     "EXISTS (SELECT 1 FROM unnest(ingredients) AS ingredient WHERE ingredient ILIKE ?)",
+                     "%#{params[:query]}%",
+                   ).order(created_at: :desc).page(params[:page]).per(20)
+               else
+                 Recipe.order(created_at: :desc).page(params[:page]).per(20)
+               end
   end
 end
